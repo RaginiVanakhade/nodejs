c66b874438f9d5bad676ca85cef133f9cb0747e7
Ragini Vanakhade <raginivankhade1203@gmail.com>
2026-08-22 00:45:18 +0530

    url shortner work

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const urlRoute = require("./routes/url")
 const {connectToMongoDB} = require("./connection")
+const URL = require("./models/url")
 
 const app = express()
 const PORT = 8000
@@ -13,4 +14,18 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
 
 
 app.use("/url", urlRoute)
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId
+  const entry =  await URL.findOneAndUpdate({
+        shortId 
+    }, {
+         $push : {
+            visitHistory : {
+                timestamp : Date.now()
+            }
+         }
+    })
+    res.redirect(entry.redirectURL)
+})
+
 app.listen(PORT, () => console.log(`server running on port http://localhost//${PORT}`))
